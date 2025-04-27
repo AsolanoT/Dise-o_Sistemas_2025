6316ee7ace45ecad27ab9e15a7340880e90ec7c5
AsolanoT <agsolano-2023a@corhuila.edu.co>
2025-04-26 22:36:10 -0500

    CREACION DE BACK Y FRONTEND

---
+++ BACKEND SPRINGBOOT/app-erp-tributario/src/main/java/com/corhuila/app_erp_tributario/AppErpTributarioApplication.java
@@ -0,0 +1,13 @@
+package com.corhuila.app_erp_tributario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppErpTributarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppErpTributarioApplication.class, args);
+	}
+
+}
---
+++ BACKEND SPRINGBOOT/app-erp-tributario/src/test/java/com/corhuila/app_erp_tributario/AppErpTributarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corhuila.app_erp_tributario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppErpTributarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
